e9e7477b6cff108e1ebde14b32b900637b5a534c
AD0791 <alexdisla1991@gmail.com>
2022-10-07 00:15:03 -0400

    poss set error handler

--- server.R
+++ server.R
@@ -1,3 +1,9 @@
 library("plumber")
 
-pr("plumber.R") %>%  pr_run(port = 8000)
+pr("plumber.R") %>%
+pr_set_error(function(req, res, err) {
+    res$status <- 500
+    list(error = "An error occurred. Please contact your administrator.")
+  }
+) %>%
+pr_run(port = 8000)
